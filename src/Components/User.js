dd3f7ae85d55b7c2a6538945eda582c6e554c2fe
Michele Boi <boimik87@gmail.com>
2019-03-12 18:35:15 +0000

    fetch

---
+++ src/Components/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const User = () => {
+  return (
+    <div className="user_container">
+      <h1>Users</h1>
+    </div>
+  );
+};
+
+export default User;
