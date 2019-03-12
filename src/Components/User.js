0892d845244e4737f64e73d6704030f64cf69d8f
Michele Boi <boimik87@gmail.com>
2019-03-12 21:51:32 +0000

    creates album div and dropdown

--- src/Components/User.js
+++ src/Components/User.js
@@ -21,21 +21,36 @@ class User extends Component {
   render() {
     const data = this.state.users;
     return (
-      <div className="user_container">
-        <h1>Users</h1>
-        {!data ? (
-          <p>...Loading</p>
-        ) : (
-          <ul>
-            {data.map((d, idx) => {
-              return (
-                <div key={idx} className="user_box">
-                  {d.name}
-                </div>
-              );
-            })}
-          </ul>
-        )}
+      <div className="main_container">
+        <div className="user_container">
+          <h1>Users</h1>
+          {!data ? (
+            <p>...Loading</p>
+          ) : (
+            <ul>
+              {data.map((d, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className="user_box"
+                    onclick={this.handleChange}
+                  >
+                    {d.name}
+                  </div>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+        <div className="album_container">
+          <select className="dropdown">
+            <option selected disabled>
+              Albums
+            </option>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+          </select>
+        </div>
       </div>
     );
   }
