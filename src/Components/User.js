33dddf8bf4d27b8f02252a0d77ac555432c26d70
Michele Boi <boimik87@gmail.com>
2019-03-14 13:40:22 +0000

    removed unnecessary div from user.js

--- src/Components/User.js
+++ src/Components/User.js
@@ -30,36 +30,34 @@ class User extends Component {
   render() {
     const userData = this.state.users; // makes it easier to access the state
     return (
-      <div className="main_container">
-        <div className="user_container">
-          <h1 className="title">Users</h1>
-          {!userData ? (
-            <p>...Loading</p>
-          ) : (
-            <ul>
-              {userData.map(userItem => {
-                return (
-                  <div
-                    key={userItem.id} // you can't render two things with the same id. either pass a key to map or do like this
-                    className="user_box"
-                    // 1) call handleClick func with the user id as parameter
-                    onClick={() => this.handleClick(userItem.id)}
-                    // check if the selectedUserId I passed from Home as props is the same as the userId and changes its colour
-                    style={{
-                      // style wants an object. backgroundcolor is a convention for react which wouldn't accept "backgroud-color"
-                      backgroundColor:
-                        this.props.selectedUserId === userItem.id // check if the selectedUserId is the same and the one we are looping in
-                          ? "lightgreen"
-                          : "white"
-                    }}
-                  >
-                    {userItem.name}
-                  </div>
-                );
-              })}
-            </ul>
-          )}
-        </div>
+      <div className="user_container">
+        <h1 className="title">Users</h1>
+        {!userData ? (
+          <p>...Loading</p>
+        ) : (
+          <ul>
+            {userData.map(userItem => {
+              return (
+                <div
+                  key={userItem.id} // you can't render two things with the same id. either pass a key to map or do like this
+                  className="user_box"
+                  // 1) call handleClick func with the user id as parameter
+                  onClick={() => this.handleClick(userItem.id)}
+                  // check if the selectedUserId I passed from Home as props is the same as the userId and changes its colour
+                  style={{
+                    // style wants an object. backgroundcolor is a convention for react which wouldn't accept "backgroud-color"
+                    backgroundColor:
+                      this.props.selectedUserId === userItem.id // check if the selectedUserId is the same and the one we are looping in
+                        ? "lightgreen"
+                        : "white"
+                  }}
+                >
+                  {userItem.name}
+                </div>
+              );
+            })}
+          </ul>
+        )}
       </div>
     );
   }
